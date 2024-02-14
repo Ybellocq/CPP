6e9bc41bb62298abf20fc491bfd34524a1cce738
Ybellocq <134070832+Ybellocq@users.noreply.github.com>
2024-02-14 15:26:57 +0100

    upgrade module cpp

---
+++ module08/ex00/easyfind.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+class OccurenceException : public std::exception{
+    public :
+        virtual const char * what () const throw (){
+            return ("No Occurence found!");
+        }    
+};
+
+template <typename T>
+int eastfind(const T & container, int nb){
+    if (std::find(container.begin(), container.end(), nb) != container.end())
+        std::cout << nb << " is in the container." << std::endl;
+    else   
+        throw  OccurenceException();
+}
